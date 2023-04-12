f02f3528a2d5a5c83f5f2eb8983e1a6e0fa3858e
matusalem.matusa22@gmail.com <jpfox.alves@gmail.com>
2023-04-12 10:08:43 -0300

    Seccond Commit

--- src/main/java/br/com/loja/modelo/Produto.java
+++ src/main/java/br/com/loja/modelo/Produto.java
@@ -15,4 +15,35 @@ public class Produto {
     private String descricao;
     private BigDecimal preco;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
 }
